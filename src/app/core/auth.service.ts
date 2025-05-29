c4a30ca0b3b6ea9a9c7d7fdd8807c5ee9b3531d9
dfriedl <s51729@edu.campus02.at>
2025-05-29 20:33:51 +0200

    add navbar

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+type Role = 'user' | 'admin' | null;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private role: Role = 'user'; // Für Test: 'admin' oder 'user'
+
+  getRole(): Role {
+    return this.role;
+  }
+
+  setRole(newRole: Role) {
+    this.role = newRole;
+  }
+
+
+  constructor() { }
+}
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {AuthService} from '../../core/auth.service';
+import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatListItem, MatNavList} from '@angular/material/list';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    MatSidenavContainer,
+    MatSidenav, MatSidenavModule, RouterOutlet, RouterLink, MatNavList, MatListItem
+
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+
+  constructor(private auth: AuthService) {
+
+
+
+  }
+
+  get role() {
+    return this.auth.getRole();
+  }
+
+}
