f79061216e804c0fe060c5d930715449b6dbf632
dfriedl <s51729@edu.campus02.at>
2025-05-29 17:24:10 +0200

    add angular material and test it

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatSlideToggle
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
