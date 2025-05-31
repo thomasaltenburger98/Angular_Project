d07d239560e247b42702468005ae56a342148d91
dfriedl <s51729@edu.campus02.at>
2025-05-31 08:41:05 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {BookListUserComponent} from './features/books/book-list-user/book-list-user.component';
+import {AuthorListUserComponent} from './features/authors/author-list-user/author-list-user.component';
+import {CartModalComponent} from './features/cart/cart-modal/cart-modal.component';
+import {BookListAdminComponent} from './features/books/book-list-admin/book-list-admin.component';
+import {AuthorListAdminComponent} from './features/authors/author-list-admin/author-list-admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'books/book-list-user', component: BookListUserComponent},
+  {path: 'authors/author-list-user', component: AuthorListUserComponent},
+  {path: 'cart/cart-modal', component: CartModalComponent},
+  {path: 'books/book-list-admin', component: BookListAdminComponent},
+  {path: 'authors/author-list-admin', component: AuthorListAdminComponent}
+];
