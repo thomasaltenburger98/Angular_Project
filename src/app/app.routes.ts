0ac10f25085580b14e3611e46404cb89305a310a
dfriedl <s51729@edu.campus02.at>
2025-06-02 22:14:29 +0200

    add book list page admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import {BookListAdminComponent} from './features/books/book-list-admin/book-list
 import {AuthorListAdminComponent} from './features/authors/author-list-admin/author-list-admin.component';
 import {HomeComponent} from './features/home/home.component';
 import {BookDetailUserComponent} from './features/books/book-detail-user/book-detail-user.component';
+import {BookEditAdminComponent} from './features/books/book-edit-admin/book-edit-admin.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'books/book-list-user', component: BookListUserComponent},
   {path: 'books/book-detail-user/:id', component: BookDetailUserComponent},
+  {path: 'books/book-edit-admin/:id', component: BookEditAdminComponent},
   {path: 'authors/author-list-user', component: AuthorListUserComponent},
   {path: 'cart/cart-modal', component: CartModalComponent},
   {path: 'books/book-list-admin', component: BookListAdminComponent},
--- src/app/features/books/book-list-admin/book-list-admin.component.ts
+++ src/app/features/books/book-list-admin/book-list-admin.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book, BookService} from '../../../core/book.service';
+import {Router} from '@angular/router';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {CurrencyPipe} from '@angular/common';
+import {MatButton, MatMiniFabButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-book-list-admin',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    CurrencyPipe,
+    MatIcon,
+    MatMiniFabButton,
+    MatButton
+  ],
   templateUrl: './book-list-admin.component.html',
   styleUrl: './book-list-admin.component.scss'
 })
-export class BookListAdminComponent {
+export class BookListAdminComponent implements OnInit {
+
+  books: Book[] = [];
+  constructor(private bookService: BookService, private router: Router) {
+  }
+
+
+  ngOnInit() :void {
+    this.bookService.getBooks().subscribe(data => this.books = data);
+  }
+
+  goToDetail(bookId: number): void {
+    this.router.navigate(['/books/book-edit-admin', bookId]);
+  }
+  addBook(): void {
+    // TODO: Navigation zu Buch-Formular (leeres Formular)
+    console.log('Buch hinzufügen – TODO: Routing oder Dialog öffnen');
+  }
+
+  deleteBook(book: Book): void {
+    // TODO: Dialog einbauen mit Bestätigung
+    console.log('Buch löschen – TODO: Buch wirklich löschen', book);
+  }
 
 }
